6288ae3ae1b0031d156dc5be0e1946dd9e6936e5
bfritscher <bfritscher@users.noreply.github.com>
2025-06-08 21:14:55 +0000

    deploy: b4570c47a2c187591d1104226fcc3f67e45cd41e

--- assets/javascript.7454abeb.js
+++
@@ -1,6 +0,0 @@
-import{conf as t,language as e}from"./typescript.e1120b82.js";import"./MonacoEditor.c4a998fc.js";import"./index.b5beb44c.js";import"./QResizeObserver.9a37a7d2.js";/*!-----------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Version: 0.33.0(4b1abad427e58dbedc1215d99a0902ffc885fcd4)
- * Released under the MIT license
- * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
- *-----------------------------------------------------------------------------*/var a=t,n={defaultToken:"invalid",tokenPostfix:".js",keywords:["break","case","catch","class","continue","const","constructor","debugger","default","delete","do","else","export","extends","false","finally","for","from","function","get","if","import","in","instanceof","let","new","null","return","set","super","switch","symbol","this","throw","true","try","typeof","undefined","var","void","while","with","yield","async","await","of"],typeKeywords:[],operators:e.operators,symbols:e.symbols,escapes:e.escapes,digits:e.digits,octaldigits:e.octaldigits,binarydigits:e.binarydigits,hexdigits:e.hexdigits,regexpctl:e.regexpctl,regexpesc:e.regexpesc,tokenizer:e.tokenizer};export{a as conf,n as language};
